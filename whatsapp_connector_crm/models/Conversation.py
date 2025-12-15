83adb70fd868e4674fda42f1ba3763704273ed35
Santiago Tojo <santiago.tojo@alpardata.com.ar>
2025-12-15 14:58:46 -0300

    [FEAT] New logic for auto lead creations

--- whatsapp_connector_crm/models/Conversation.py
+++ whatsapp_connector_crm/models/Conversation.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 
 class AcruxChatConversation(models.Model):
@@ -12,3 +12,28 @@ class AcruxChatConversation(models.Model):
         out = super(AcruxChatConversation, self).get_fields_to_read()
         out.extend(['crm_lead_id'])
         return out
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        conversations = super(AcruxChatConversation, self).create(vals_list)
+        for conv in conversations:
+            if not conv.crm_lead_id and conv.conv_type == 'normal':
+                lead_vals = {
+                    'name': _('WhatsApp: %s') % (conv.name or conv.number),
+                    'partner_id': conv.res_partner_id.id,
+                    'phone': conv.number,
+                    'mobile': conv.number,
+                    'user_id': conv.agent_id.id or self.env.user.id,
+                    'type': 'lead',
+                }
+                lead = self.env['crm.lead'].create(lead_vals)
+                conv.crm_lead_id = lead.id
+        return conversations
+
+    def write(self, vals):
+        res = super(AcruxChatConversation, self).write(vals)
+        if 'res_partner_id' in vals:
+            for conv in self:
+                if conv.crm_lead_id and not conv.crm_lead_id.partner_id and vals['res_partner_id']:
+                    conv.crm_lead_id.partner_id = vals['res_partner_id']
+        return res
